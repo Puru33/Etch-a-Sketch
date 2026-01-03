7ce570167496b707504dea5c9c5d2743fafa9a76
Puru33 <90132747+Puru33@users.noreply.github.com>
2026-01-03 07:18:56 +0530

    Add code

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ function createGrid(size) {
     }
 }
 
+function changeColor(e) {
+
+    const randomR = Math.floor(Math.random() * 256)
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+}
+
+resetBtn.addEventListener('click', () => {
+    let newSize = prompt("Enter new grid size (max 100):")
+
+    newSize = parseInt(newSize)
+
+    if (newSize > 0 && newSize <= 100){
+        createGrid(newSize)
+    } else {
+        alert ("Please enter a valid number between 1 and 100.")
+    }
+})
+
+createGrid(16)
